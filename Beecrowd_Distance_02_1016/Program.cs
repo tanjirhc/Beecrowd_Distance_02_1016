ed43535f5f6cb5882f1e1d8d913582086f164e03
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-03-12 18:49:29 +0600

    Add project files.

---
+++ Beecrowd_Distance_02_1016/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beecrowd_Distance_02_1016
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, distance;
+
+            x = int.Parse(Console.ReadLine());
+
+            distance = (x *2);
+
+            Console.WriteLine("{0} minutos", distance);
+            Console.ReadKey();
+        }
+    }
+}
